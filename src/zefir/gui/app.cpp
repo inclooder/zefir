4af88ff01c397b7e32ae63708778230f49d62fb2
Mateusz Kluge <inclooder@gmail.com>
2018-06-04 00:03:15 +0200

    Check password

--- src/zefir/gui/app.cpp
+++ src/zefir/gui/app.cpp
@@ -5,23 +5,39 @@ namespace Zefir::Gui {
   App::App(int argc, char **argv) : argc(argc), argv(argv) {
   }
 
+  App::~App() {
+    delete passwordWindow;
+  }
+
   int App::run() {
+    db = std::make_shared<SqlCipher::Connection>("zefir.db");
+
     int argcForGtk = 1;
     auto app = Gtk::Application::create(argcForGtk, argv, "org.gtkmm.examples.base");
+    initializeWidgets();
 
-    Gtk::Window * window = nullptr;
-    auto builder = Gtk::Builder::create_from_file("res/gui.glade");
-    builder->get_widget("master_password_window", window);
-
-    Gtk::Entry * passwordEntry = nullptr;
-    builder->get_widget("password_entry", passwordEntry);
-    passwordEntry->set_events(Gdk::ALL_EVENTS_MASK);
-    passwordEntry->signal_activate().connect(sigc::mem_fun(*this, &App::onEnterKeyPressed));
-
-    return app->run(*window);
+    return app->run(*passwordWindow);
   }
 
   void App::onEnterKeyPressed() {
-    std::cout << "checking password" << std::endl;
+    std::string masterPassword = passwordEntry->get_text();
+    if(!db->setPassword(masterPassword)) {
+      std::cout << "invalid password" << std::endl;
+      Gtk::MessageDialog dialog(*passwordWindow, "Incorrect password.", false, Gtk::MessageType::MESSAGE_ERROR);
+      dialog.run();
+      passwordEntry->set_text("");
+    } else {
+      std::cout << "valid password" << std::endl;
+      //Pokazac liste haseł
+      passwordWindow->hide();
+    }
+  }
+
+  void App::initializeWidgets() {
+    auto builder = Gtk::Builder::create_from_file("res/gui.glade");
+    builder->get_widget("master_password_window", passwordWindow);
+    builder->get_widget("password_entry", passwordEntry);
+    passwordEntry->set_events(Gdk::ALL_EVENTS_MASK);
+    passwordEntry->signal_activate().connect(sigc::mem_fun(*this, &App::onEnterKeyPressed));
   }
 };
--- src/zefir/gui/app.hpp
+++ src/zefir/gui/app.hpp
@@ -2,16 +2,23 @@
 #define ZEFIR_GUI_APP_H
 
 #include <gtkmm.h>
+#include <memory>
+#include "sql_cipher/connection.hpp"
 
 namespace Zefir::Gui {
   class App {
     public:
       App(int argc, char **argv);
+      ~App();
       int run();
       void onEnterKeyPressed();
+      void initializeWidgets();
     private:
       int argc;
       char **argv;
+      Gtk::Window * passwordWindow;
+      Gtk::Entry * passwordEntry;
+      std::shared_ptr<SqlCipher::Connection> db;
   };
 };
 
